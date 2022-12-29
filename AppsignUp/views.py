111ccd51136104c95ce0a112eef5cc6d7d018a99
Leandro Montesoro <montesoro.leandro@gmail.com>
2022-12-29 11:36:51 -0300

    nueva branch

--- AppsignUp/views.py
+++ AppsignUp/views.py
@@ -16,7 +16,7 @@ def register(request):
             username=form.cleaned_data.get("username")
             form.save()
             #aca se podria loguear el usuario, con authenticate y login... pero no lo hago
-            return render(request, "../AppMain/padre.html", {"mensaje":f"Usuario {username} creado correctamente"})
+            return render(request, "", {"mensaje":f"Usuario {username} creado correctamente"})
         else:
             return render(request, "index.html", {"form":form, "mensaje":"Error al crear el usuario"})
      
